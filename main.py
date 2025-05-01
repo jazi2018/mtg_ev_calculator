5368c0215a5776026fe8d1ef939d7957b38875a4
jazi2018 <jaredaziv@gmail.com>
2025-05-01 10:24:24 -0700

    almost done with base - swapping computers so pushing

--- main.py
+++ main.py
@@ -12,11 +12,56 @@ headers = {
     'accept' : '*/*'
 }
 
-def extract_prices(data) -> list:
-    pass
-    # prices = []
-    # while True:
+def extract_data(data: list, keys:list = ['name', 'rarity']) -> list:
+    '''
+    Extracts relevant data from json formatted scryfall query
+    
+    :param data list: A list of dictionaries returned from a scryfall query
+    :param keys list: Any number of desired keys to be extracted from query - price exclusive.
+    If no keys are passed, default values are used.
+    :returns: A list of dictionaries containing name, price (usd), and rarity for each card by default
+    '''
+    new_data = []
+
+    try:
+        for card in data:
+            temp = {}
+            temp['price'] = data['prices']['usd']
+            for key in keys:
+                temp[key] = data[key]
+            
+            new_data.append(temp)
+
+    except KeyError as e:
+        print(f'An invalid key was selected: {e}')
+        return new_data
+    
+    return new_data
+
+
+def get_data_from_set(set: str) -> list:
+    data = []
+    query = f'https://api.scryfall.com/cards/search?q=set:{set}+-type:land'
+    while True:
+        try:
+            #query scryfall for set
+            r = requests.get(query, headers=headers)
+            r.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f'An error has occured during scryfall query: {e}')
+            return data
         
+        #convert query to json
+        page = r.json()
+        #extract data from this page's list of cards
+        data.extend(extract_data(page['data']))
+
+        if page['has_more']:
+            #set query for next page, repeat loop
+            query = page['next_page']
+        else:
+            #no more pages, query done - return data
+            return data
 
 def main():
     request_delay = 0.075
